2b0fc7b4442c56f74ba027d24552793e32598ec6
shimaa <shimaa.rmadan87@gmail.com>
2021-02-05 02:12:04 +0200

    first bootstrap

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// $(window).scroll(function(){
+//     let  windowscroll  =  $(window).scrollTop()
+
+//         console.log(windowscroll)
+//     let aboutoffset  = $("#about").offset().top
+
+//        if (windowscroll >= aboutoffset)
+//        {
+//            $("nav").css("backgroundColor",red);
+//            transition:all   1s
+
+//        }
+//        else{
+//         $("nav").css("backgroundColor",trasparent);
+//         transition:all   1s
+//        }
+
+
+
+
+    
+// })
+
+// console.log("hi")
+$(window).scroll(function(){
+    $(window).scrollTop()
+})
